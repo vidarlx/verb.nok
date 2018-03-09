2ee4882620f874e282c826bc428c318b38c87a29
vidarlx <lax@fea.st>
2018-03-10 00:05:31 +0100

    Basic search is working now

--- src/containers/word.js
+++ src/containers/word.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchVerbs } from '../actions';
 
 class Word extends Component {
+  constructor(props) {
+    super(props);
+
+    this.fetchWord = this.fetchWord.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchVerbs();
+  }
+
+  fetchWord() {
+    const { verbs } = this.props;
+    const current = verbs['_current'];
+    return {
+      verb: verbs[current] || false,
+      key: current
+    }
+  }
+
   render() {
+    const found = this.fetchWord();
+
     return (
-      <div> Word </div>
+      <div>
+        <h2>{found.key}</h2>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Present</th>
+              <th>Past</th>
+              <th>Perfect</th>
+              <th>Meaning PL</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{found.verb.present}</td>
+              <td>{found.verb.past}</td>
+              <td>{found.verb.perfect}</td>
+              <td>{found.verb.meaning_pl}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
 
-export default Word;
+function mapStateToProps({ verbs }) {
+  return { verbs };
+}
+
+export default connect(mapStateToProps, { fetchVerbs })(Word);
