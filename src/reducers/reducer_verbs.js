239a4b4978a3394d14eebc67054fcfb55302dc55
vidarlx <lax@fea.st>
2018-03-11 02:20:06 +0100

    Separate reducers for list and active verbs

--- src/reducers/reducer_verbs.js
+++ src/reducers/reducer_verbs.js
@@ -1,15 +1,9 @@
-import { FETCH_VERBS, FIND_VERB } from '../actions';
+import { FETCH_VERBS } from '../actions';
 
 export default function (state = {}, action) {
   switch (action.type) {
     case FETCH_VERBS:
-      return action.payload.data
-
-    case FIND_VERB:
-      return { ...state, _current: action.verb }
-
-    default:
-      console.log('Unknown action: ', action.type)
+      return action.payload.data;
   }
   return state;
 }
