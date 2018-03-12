45a23fd2ca2a2ad2b4debd72f8f9f0b664dd4991
vidarlx <lax@fea.st>
2018-03-13 00:52:42 +0100

    Show random verb

--- src/reducers/reducer_active_verbs.js
+++ src/reducers/reducer_active_verbs.js
@@ -3,7 +3,9 @@ import { FIND_VERB } from '../actions';
 export default function (state = [], action) {
   switch (action.type) {
     case FIND_VERB:
-      return [ ...state, action.verb ];
+      if (action.verb) {
+        return [ ...state, action.verb ];
+      }
   }
   return state;
 }
