a7bcfee79c0a97d3cc21e08e0386033fa0e88d29
Sean Doe <lax@fea.st>
2018-08-09 20:58:40 +0200

    Show polish verb coniungation

--- src/components/verb_table.js
+++ src/components/verb_table.js
@@ -4,6 +4,18 @@ import { connect } from 'react-redux';
 
 import { findVerbDetails } from '../actions';
 
+class TableRow extends Component {
+  render() {
+    return (
+      <tr>
+        <th scope="row">{this.props.person}</th>
+        <td>{this.props.past}</td>
+        <td>{this.props.present}</td>
+      </tr>
+    )
+  }
+}
+
 class VerbTable extends Component {
   componentDidMount() {
     const verb = this.props.id;
@@ -11,60 +23,55 @@ class VerbTable extends Component {
   }
 
   render () {
-    console.log(this.props)
     if (!this.props.verb) {
       return (
         <p>Vente...</p>
       )
     }
     const { verb } = this.props;
+    console.log(verb);
+    const persons =  {
+      ja: "ja",
+      ty: "ty",
+      on: 'ona/ona/ono',
+      my: "my",
+      wy: "wy",
+      oni: 'oni/one'
+    };
+
+    const rows = () => {
+      const rows = [];
+      const keys = Object.keys(persons);
+      for (let i in persons) {
+        let person = persons[i];
+        rows.push(
+          <TableRow key={i}
+            person={person}
+            past={verb.polsk.przeszły[i]} 
+            present={verb.polsk.terazniejszy[i]} 
+          />    
+        )
+      }
+      return rows;
+    }
+
     return (
       <div>
         <span className="input-group-btn">
-          <Link className="btn btn-success" to="/">powrót</Link>
+          <Link className="btn btn-success margin-bottom" to="/">powrót</Link>
         </span>
 
         <table className="table">
           <thead>
             <tr>
-              <th scope="col">#</th>
+              <th scope="col"></th>
               <th scope="col">Czas przeszły</th>
               <th scope="col">Czas terazniejszy</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <th scope="row">Ja</th>
-              <td>{verb.polsk.przeszły.ja}</td>
-              <td>{verb.polsk.terazniejszy.ja}</td>
-            </tr>
-            <tr>
-              <th scope="row">Ty</th>
-              <td>{verb.polsk.przeszły.ty}</td>
-              <td>{verb.polsk.terazniejszy.ty}</td>
-            </tr>
-            <tr>
-              <th scope="row">On/ona/ono</th>
-              <td>{verb.polsk.przeszły.on}</td>
-              <td>{verb.polsk.terazniejszy.on}</td>
-            </tr>
-            <tr>
-              <th scope="row">My</th>
-              <td>{verb.polsk.przeszły.my}</td>
-              <td>{verb.polsk.terazniejszy.my}</td>
-            </tr>
-            <tr>
-              <th scope="row">Wy</th>
-              <td>{verb.polsk.przeszły.wy}</td>
-              <td>{verb.polsk.terazniejszy.wy}</td>
-            </tr>
-            <tr>
-              <th scope="row">Oni/One</th>
-              <td>{verb.polsk.przeszły.oni}</td>
-              <td>{verb.polsk.terazniejszy.oni}</td>
-            </tr>
-          </tbody>
+          {rows()}
         </table>
+
       </div>
     )
   }
