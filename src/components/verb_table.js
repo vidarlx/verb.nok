991299d47848b92e49e5678dbefa49d94790e65d
vidarlx <lax@fea.st>
2018-08-26 20:51:06 +0200

    Future polish form of a verb added

--- src/components/verb_table.js
+++ src/components/verb_table.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import Collapse, { Panel } from 'rc-collapse';
-import 'rc-collapse/assets/index.css';
+import Collapse, { Panel } from "rc-collapse";
+import "rc-collapse/assets/index.css";
 
-import { findVerbDetails } from '../actions';
+import { findVerbDetails } from "../actions";
 
 class VerbTable extends Component {
   componentWillMount() {
@@ -17,199 +17,237 @@ class VerbTable extends Component {
 
   render() {
     if (!this.props.verb) {
-      return (
-        <img src="../../assets/spinner.gif" className="spinner" />
-      )
+      return <img src="../../assets/spinner.gif" className="spinner" />;
     }
     const { verb } = this.props;
 
     return (
       <div>
         <h3 className="languages">
-          <img src="../../assets/polska.png" />{verb.polsk_verb} &rarr;
-          <img src="../../assets/norge.png" />{verb.norsk_verb}
+          <img src="../../assets/polska.png" />
+          {verb.polsk_verb} &rarr;
+          <img src="../../assets/norge.png" />
+          {verb.norsk_verb}
         </h3>
-        {renderPolishTable(verb)}
-        {renderNorwegianTable(verb)}
-
+        {renderTables(verb)}
       </div>
-    )
+    );
   }
 }
 
-function renderPolishTable(verb) {
+function renderTables(verb) {
   return (
     <div className="container">
       <Collapse accordion={true}>
-        <Panel header="Czas przeszły" headerClass="tenseHeader past">
-          <div className="row">
-            <div className="col-4">ja</div>
-            <div className="col">{verb.polsk.przeszły.ja}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">ty</div>
-            <div className="col">{verb.polsk.przeszły.ty}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">on</div>
-            <div className="col">{verb.polsk.przeszły.on}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">ona</div>
-            <div className="col">{verb.polsk.przeszły.ona}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">ono</div>
-            <div className="col">{verb.polsk.przeszły.ono}</div>
-          </div>
-          <hr />
-          <div className="row">
-            <div className="col-4">my</div>
-            <div className="col">{verb.polsk.przeszły.my}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">wy</div>
-            <div className="col">{verb.polsk.przeszły.wy}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">oni</div>
-            <div className="col">{verb.polsk.przeszły.oni}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">one</div>
-            <div className="col">{verb.polsk.przeszły.one}</div>
-          </div>
-        </Panel>
-        <Panel header="Czas teraźniejszy" headerClass="tenseHeader">
-          <div className="row">
-            <div className="col-4">ja</div>
-            <div className="col">{verb.polsk.terazniejszy.ja}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">ty</div>
-            <div className="col">{verb.polsk.terazniejszy.ty}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">on</div>
-            <div className="col">{verb.polsk.terazniejszy.on}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">ona</div>
-            <div className="col">{verb.polsk.terazniejszy.ona}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">ono</div>
-            <div className="col">{verb.polsk.terazniejszy.ono}</div>
-          </div>
-          <hr />
-          <div className="row">
-            <div className="col-4">my</div>
-            <div className="col">{verb.polsk.terazniejszy.my}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">wy</div>
-            <div className="col">{verb.polsk.terazniejszy.wy}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">oni</div>
-            <div className="col">{verb.polsk.terazniejszy.oni}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">one</div>
-            <div className="col">{verb.polsk.terazniejszy.oni}</div>
-          </div>
-        </Panel>
-        <Panel header="Czas przyszły" headerClass="tenseHeader">
-          <div className="row">
-            <div className="col-4">ja</div>
-            <div className="col">{verb.polsk.przyszly.ja}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">ty</div>
-            <div className="col">{verb.polsk.przyszly.ty}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">on</div>
-            <div className="col">{verb.polsk.przyszly.on}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">ona</div>
-            <div className="col">{verb.polsk.przyszly.ona}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">ono</div>
-            <div className="col">{verb.polsk.przyszly.ono}</div>
-          </div>
-          <hr />
-          <div className="row">
-            <div className="col-4">my</div>
-            <div className="col">{verb.polsk.przyszly.my}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">wy</div>
-            <div className="col">{verb.polsk.przyszly.wy}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">oni</div>
-            <div className="col">{verb.polsk.przyszly.oni}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">one</div>
-            <div className="col">{verb.polsk.przyszly.oni}</div>
-          </div>
-        </Panel>
-        <Panel header="Bokmål" headerClass="tenseHeader">
-          <div className="row">
-            <div className="col-4">Grunnform</div>
-            <div className="col">{verb.bokmal.grunnform}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">Presens</div>
-            <div className="col">{verb.bokmal.presens}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">Preteritum</div>
-            <div className="col">{verb.bokmal.preteritum}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">Perfektum</div>
-            <div className="col">{verb.bokmal.perfektum}</div>
-          </div>
-        </Panel>
-        <Panel header="Nynorsk" headerClass="tenseHeader">
-          <div className="row">
-            <div className="col-4">Grunnform</div>
-            <div className="col">{verb.nynorsk.grunnform}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">Presens</div>
-            <div className="col">{verb.nynorsk.presens}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">Preteritum</div>
-            <div className="col">{verb.nynorsk.preteritum}</div>
-          </div>
-          <div className="row">
-            <div className="col-4">Perfektum</div>
-            <div className="col">{verb.nynorsk.perfektum}</div>
-          </div>
-        </Panel>
+        {renderPolishPast(verb)}
+        {renderPolishPresent(verb)}
+        {renderPolishFuture(verb)}
+        {renderBokmalTable(verb)}
+        {renderNynorskTable(verb)}
       </Collapse>
     </div>
-  )
+  );
 }
 
-function renderNorwegianTable(verb) {
+function renderBokmalTable(verb) {
   return (
-    <div></div>
-  )
+    <Panel header="Bokmål" headerClass="tenseHeader">
+      <div className="row">
+        <div className="col-4">Grunnform</div>
+        <div className="col">{verb.bokmal.grunnform}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">Presens</div>
+        <div className="col">{verb.bokmal.presens}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">Preteritum</div>
+        <div className="col">{verb.bokmal.preteritum}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">Perfektum</div>
+        <div className="col">{verb.bokmal.perfektum}</div>
+      </div>
+    </Panel>
+  );
 }
 
+function renderNynorskTable(verb) {
+  return (
+    <Panel header="Nynorsk" headerClass="tenseHeader">
+      <div className="row">
+        <div className="col-4">Grunnform</div>
+        <div className="col">{verb.nynorsk.grunnform}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">Presens</div>
+        <div className="col">{verb.nynorsk.presens}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">Preteritum</div>
+        <div className="col">{verb.nynorsk.preteritum}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">Perfektum</div>
+        <div className="col">{verb.nynorsk.perfektum}</div>
+      </div>
+    </Panel>
+  );
+}
+
+function renderPolishPast(verb) {
+  return (
+    <Panel header="Czas przeszły" headerClass="tenseHeader past">
+      <div className="row">
+        <div className="col-4">ja</div>
+        <div className="col">{verb.polsk.przeszły.ja}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">ty</div>
+        <div className="col">{verb.polsk.przeszły.ty}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">on</div>
+        <div className="col">{verb.polsk.przeszły.on}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">ona</div>
+        <div className="col">{verb.polsk.przeszły.ona}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">ono</div>
+        <div className="col">{verb.polsk.przeszły.ono}</div>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col-4">my</div>
+        <div className="col">{verb.polsk.przeszły.my}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">wy</div>
+        <div className="col">{verb.polsk.przeszły.wy}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">oni</div>
+        <div className="col">{verb.polsk.przeszły.oni}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">one</div>
+        <div className="col">{verb.polsk.przeszły.one}</div>
+      </div>
+    </Panel>
+  );
+}
+
+function renderPolishPresent(verb) {
+  return (
+    <Panel header="Czas teraźniejszy" headerClass="tenseHeader">
+      <div className="row">
+        <div className="col-4">ja</div>
+        <div className="col">{verb.polsk.terazniejszy.ja}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">ty</div>
+        <div className="col">{verb.polsk.terazniejszy.ty}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">on</div>
+        <div className="col">{verb.polsk.terazniejszy.on}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">ona</div>
+        <div className="col">{verb.polsk.terazniejszy.ona}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">ono</div>
+        <div className="col">{verb.polsk.terazniejszy.ono}</div>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col-4">my</div>
+        <div className="col">{verb.polsk.terazniejszy.my}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">wy</div>
+        <div className="col">{verb.polsk.terazniejszy.wy}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">oni</div>
+        <div className="col">{verb.polsk.terazniejszy.oni}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">one</div>
+        <div className="col">{verb.polsk.terazniejszy.oni}</div>
+      </div>
+    </Panel>
+  );
+}
+
+function renderPolishFuture(verb) {
+  // standard be + basic form of a verb
+  if (!verb.polsk.przyszly) {
+    verb.polsk.przyszly = {
+      ja: `będę ${verb.polsk_verb}`,
+      ty: `będziesz ${verb.polsk_verb}`,
+      on: `będzie ${verb.polsk_verb}`,
+      ona: `będzie ${verb.polsk_verb}`,
+      ono: `będzie ${verb.polsk_verb}`,
+      my: `będziemy ${verb.polsk_verb}`,
+      wy: `będziecie ${verb.polsk_verb}`,
+      oni: `bedą ${verb.polsk_verb}`,
+      one: `będą ${verb.polsk_verb}`
+    }
+  }
+  return (
+    <Panel header="Czas przyszły" headerClass="tenseHeader">
+      <div className="row">
+        <div className="col-4">ja</div>
+        <div className="col">{verb.polsk.przyszly.ja}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">ty</div>
+        <div className="col">{verb.polsk.przyszly.ty}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">on</div>
+        <div className="col">{verb.polsk.przyszly.on}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">ona</div>
+        <div className="col">{verb.polsk.przyszly.ona}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">ono</div>
+        <div className="col">{verb.polsk.przyszly.ono}</div>
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col-4">my</div>
+        <div className="col">{verb.polsk.przyszly.my}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">wy</div>
+        <div className="col">{verb.polsk.przyszly.wy}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">oni</div>
+        <div className="col">{verb.polsk.przyszly.oni}</div>
+      </div>
+      <div className="row">
+        <div className="col-4">one</div>
+        <div className="col">{verb.polsk.przyszly.oni}</div>
+      </div>
+    </Panel>
+  );
+}
 
 const mapStateToProps = state => {
   return {
     verb: state.verbs.chosen
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, { findVerbDetails })(VerbTable);
+export default connect(
+  mapStateToProps,
+  { findVerbDetails }
+)(VerbTable);
